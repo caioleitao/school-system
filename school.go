2a2fd338128ea3b7043aa71edfe56d7d8a38e5a9
Caio César <br.caiorecife@cesarleitao.com.br>
2019-08-09 09:54:23 -0300

    files

--- school.go
+++ school.go
@@ -3,16 +3,23 @@ package main
 import "fmt"
 
 func main() {
+	var student string
 	var n1 float64
 	var n2 float64
 
-
+	fmt.Println("Digite o nome do aluno: ")
+	fmt.Scan(&student)
 	fmt.Println("Digite a nota do primeiro semestre: ")
 	fmt.Scanf("%f", &n1)
 	fmt.Println("Digite a nota do segundo semestre: ")
 	fmt.Scanf("%f", &n2)
 	note := (n1 + n2) / 2
-	fmt.Println("A nota final do aluno foi:", note)
+
+	if note < 7 {
+		fmt.Println("A nota final de", student, "foi:", note, "[REPROVADO]")
+	} else {
+		fmt.Println("A nota final de", student, "foi:", note, "[APROVADO]")
+	}
 
 
 }
