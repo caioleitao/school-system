018ca72652c194db3f5f3733400fb2c0e7621c03
Caio César <br.caiorecife@cesarleitao.com.br>
2020-01-25 15:35:34 -0300

    Update school.go

--- school.go
+++ school.go
@@ -7,18 +7,18 @@ func main() {
 	var n1 float64
 	var n2 float64
 
-	fmt.Println("Digite o nome do aluno: ")
+	fmt.Println("Enter student name: ")
 	fmt.Scan(&student)
-	fmt.Println("Digite a nota do primeiro semestre: ")
+	fmt.Println("Enter the first semester grade: ")
 	fmt.Scanf("%f", &n1)
-	fmt.Println("Digite a nota do segundo semestre: ")
+	fmt.Println("Enter the second semester grade: ")
 	fmt.Scanf("%f", &n2)
 	note := (n1 + n2) / 2
 
 	if note < 7 {
-		fmt.Println("A nota final de", student, "foi:", note, "[REPROVADO]")
+		fmt.Println("The final grade of ", student, "was:", note, "(DISAPPROVED)")
 	} else {
-		fmt.Println("A nota final de", student, "foi:", note, "[APROVADO]")
+		fmt.Println("The final grade of ", student, "was:", note, "(APPROVED)")
 	}
 
 
